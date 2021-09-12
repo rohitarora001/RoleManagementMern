791812f993430d2c8546088525b3b172d389ecd4
rohitarora007 <rohit4019545@jmit.ac.in>
2021-09-12 10:31:26 +0530

    Final try

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ module.exports = {
   baseUrl : "role-management-mern.herokuapp.com/",
   JWT_SECRET:"thelongerthebetter",
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 }
